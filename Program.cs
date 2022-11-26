d15f6c70e3277cce9e6ccb87512c091d1aa8d76e
Anna <sviridova_anya@mail.ru>
2022-11-26 22:41:38 +0300

    напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>0)
+{
+    while (number/10>=100)
+    {
+        number=number/10;
+    }
+    if (number/10<10)
+    {
+        Console.WriteLine($"Третьей цифры у числа нет");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра заданного числа равна {number%10}");
+    }
+}
+else
+{
+    while (number/10<=-100)
+    {
+        number=number/10;
+    }
+    if (number/10>-10)
+    {
+        Console.WriteLine($"Третьей цифры у числа нет");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра заданного числа равна {-number%10}");
+    }
+}
